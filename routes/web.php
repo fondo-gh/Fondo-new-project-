2118e01a53a9afcfbbec120de6ec3f52bc2443c1
Francis Deh <dfdemajesty699@gmail.com>
2019-12-01 17:11:13 +0000

    Implemented Admin Scaffold

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,32 @@
 |
 */
 
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+
+//Welcome route
 Route::get('/', function () {
     return view('welcome');
 });
 
+//auth routes
 Auth::routes();
 
+//page routes
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/dashboard', 'AdminController@index')->name('dashboard');
+
+
+/*Admin routes*/
+/* Admin Routes for Authentication*/
+Route::get('admin/login', 'Admin\LoginController@showLoginForm')->name('admin.login');
+Route::post('admin/login', 'Admin\LoginController@login')->name('admin.login');
+Route::post('admin/logout', 'Admin\LoginController@logout')->name('admin.logout');
+Route::get('admin/register', 'Admin\RegisterController@showRegistrationForm')->name('admin.register');
+Route::post('admin/register', 'Admin\RegisterController@register')->name('admin.register');
+Route::post('admin/password/email', 'Admin\ForgotPasswordController@sendResetLinkEmail')->name('admin.password.email');
+Route::get('admin/password/reset', 'Admin\ForgotPasswordController@showLinkRequestForm')->name('admin.password.request');
+Route::post('admin/password/reset', 'Admin\ResetPasswordController@reset');
+Route::get('admin/password/reset/{token}', 'Admin\ResetPasswordController@showResetForm')->name('admin.password.reset');
+
